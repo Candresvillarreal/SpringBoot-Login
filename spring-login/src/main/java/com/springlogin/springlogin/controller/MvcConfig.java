e0bc71607590b6d3971e22d021eb87c4cbef80e7
Carlos Andrés Villarreal <candresvillarreal@gmail.com>
2022-12-27 10:57:31 +0100

    SpringBoot Login

---
+++ spring-login/src/main/java/com/springlogin/springlogin/controller/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.springlogin.springlogin.controller;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
